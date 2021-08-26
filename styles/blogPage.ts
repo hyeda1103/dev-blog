6a07c9f20d6a8efc94c86e1b5c4c784334405939
hyeda1103 <dalgona92@gmail.com>
2021-08-26 23:25:16 +0900

    styles for blogPage

--- styles/blogPage.ts
+++ styles/blogPage.ts
@@ -3,4 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 3fr 1fr;
+  padding: 0 0.5rem;
 `
+
+export const PostBlock = styled.div``
