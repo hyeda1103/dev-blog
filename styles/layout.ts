fa8d0112734c55424afe5441191fd6110b2f3d16
hyeda1103 <dalgona92@gmail.com>
2021-08-28 15:43:39 +0900

    add styles for basic layout

--- styles/layout.ts
+++ styles/layout.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const Main = styled.main`
-  width: 960px;
+  width: 1200px;
   margin: 0 auto;
-  padding: 4rem 0;
+  padding: 8rem 0;
   min-height: 100vh;
 `
