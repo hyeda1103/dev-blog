d18dc6e890575a1466ba43b81fb25639179d63e6
hyeda1103 <dalgona92@gmail.com>
2021-08-29 22:37:37 +0900

    add styles for categoryList

---
+++ styles/categoryList.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0.5rem;
+`
+
+export const List = styled.ul``
+
+export const Item = styled.li`
+  list-style: none;
+  padding: 3px 8px;
+  background: #f2f2f2;
+  font-size: 11px;
+  border-radius: 5px;
+  color: #040a3c;
+  display: inline-block;
+  margin: 0.5rem;
+`
