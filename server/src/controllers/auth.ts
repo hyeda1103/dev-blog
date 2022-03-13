64cb839538b86790a64665c32d0ceef9f260acac
hyeda1103 <dalgona92@gmail.com>
2022-03-13 18:48:12 +0900

    feat: AWS SES for sending email

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,5 +1,46 @@
 import { Request, Response } from 'express'
+import AWS from 'aws-sdk'
+
+
 
 export const register = (req: Request, res: Response) => {
-  console.log('REGISTER CONTROLLER', req.body)
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  });
+  const { name, email, password } = req.body;
+  // Example code for SES:
+  // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ses-examples-sending-email.html
+  const params = {
+    Source: `${process.env.EMAIL_FROM}`, /** SENDER_EMAIL_ADDRESS */
+    Destination: {
+      ToAddresses: [email]
+    },
+    ReplyToAddresses: [`${process.env.EMAIL_TO}`],
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: `<html><body><h1>Hello, ${name}</h1></body></html>`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Complete your registration'
+      }
+    }
+  }
+  
+  const sendEmailonRegister = new AWS.SES({
+    apiVersion: '2010-12-01'
+  }).sendEmail(params).promise()
+  
+  sendEmailonRegister.then(data => {
+    console.log('Email submitted to SES', data)
+    res.send('Email sent')
+  }).catch(error => {
+    console.log('SES email on register', error)
+    res.send('Email failed')
+  })
 }
