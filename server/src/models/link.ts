288ac7770ddb74f39b643426e29bbfd17a669985
hyeda1103 <dalgona92@gmail.com>
2022-03-30 17:06:52 +0900

    feat: link schema

---
+++ server/src/models/link.ts
@@ -0,0 +1,52 @@
+import mongoose, {
+  Schema, model,
+} from "mongoose";
+
+const linkSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 256
+  },
+  url: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 256
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    index: true
+  },
+  postedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  categories: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }],
+  type: {
+    type: String,
+    default: 'Free'
+  },
+  medium: {
+    type: String,
+    default: 'Video'
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  }
+}, {
+  timestamps: true
+})
+
+// export link model
+const LinkModel = model('Link', linkSchema);
+
+export default LinkModel; 
