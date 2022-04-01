6dae1bd1d0cc56a27633874e9972cb6deff7cc32
hyeda1103 <dalgona92@gmail.com>
2022-04-02 01:22:30 +0900

    feat: sticky aside

--- client/components/templates/twoCol/styles.ts
+++ client/components/templates/twoCol/styles.ts
@@ -5,13 +5,18 @@ interface StyleProps {
 }
 
 export const GridLayout = styled.section<StyleProps>`
+  position: relative;
   display: grid;
   grid-template-columns: 3fr 1fr;
   column-gap: 24px;
 `
 
 export const MainCol = styled.main`
+  position: relative;
 `;
 
 export const SubCol = styled.aside`
+  position: sticky;
+  align-self: start;
+  top: 112px;
 `;
