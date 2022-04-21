1d77482c3c7d37866ef6f9b83589345b62b560e2
hyeda1103 <dalgona92@gmail.com>
2022-04-21 22:57:35 +0900

    feat: resume

---
+++ client/pages/resume/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '@/components/templates/layout';
+
+function ResumePage() {
+  return (
+    <Layout>ResumePage</Layout>
+  )
+}
+
+export default ResumePage;
