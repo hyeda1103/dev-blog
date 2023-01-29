d6e4ad6f4a896f97d9f51ccf7fe09db22835ce33
hyeda1103 <dalgona92@gmail.com>
2023-01-29 13:15:24 +0900

    fix: responsive home page

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "dev-blog",
-    // API: "http://localhost:8000/api",
-    API: "/api",
+    API: "http://localhost:8000/api",
+    // API: "/api",
     PRODUCTION: true,
     DOMAIN: "http://43.200.72.175",
     FB_APP_ID: "JADLKMJKMCAIDK",
